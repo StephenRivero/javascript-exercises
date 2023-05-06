2ce69ae9949edf2b9d034c3ba4c9b8b30933c883
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-05-06 11:32:49 +0800

    Solve leapYears Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, multiply) {
+   
+    if (multiply < 0) {
+        return "ERROR"
+    }
 
+    let string = "";
+
+    for (i = 0; i < multiply; i++){
+     string += word;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
